72a538f40f3ad1b6fb77a28574d32dc5968e07bb
Roy Gardiner <roy.gardiner@btopenworld.com>
2014-01-24 13:31:03 +0000

    Much simplified version; data driven, not logic driven

--- lib/user.rb
+++ lib/user.rb
@@ -3,16 +3,28 @@ class User
 	def initialize
 		@rank = -8
 		@progress = 0
+		
 		@ranks = {  
                -8 => 1, -7 => 2, -6 => 3, -5 => 4, -4 => 5, -3 => 6, -2 => 7, -1 => 8,
                 1 => 9,  2 => 10, 3 => 11, 4 => 12, 5 => 13, 6 => 14, 7 => 15, 8 => 16
   						}
+		@level_difference_uplift =
+							{
+  	             -1 =>    0,  0 =>    3,     
+  	              1 =>   10,  2 =>   40,    3 => 90,  4 =>  160,  5 => 250,
+  	              6 =>  360,  7 =>  490,   8 => 640,  9 =>  810, 10 => 1000,
+  	             11 => 1210, 12 => 1440, 13 => 1690, 14 => 1960, 15 => 2250
+  	          }
   	@to_ranks = { 
-                1  => -8,  2 => -7, 3  => -6, 4  => -5, 5  => -4, 6  => -3, 7  => -2, 8  => -1,
-                9  =>  1, 10 =>  2, 11 =>  3, 12 =>  4, 13 =>  5, 14 =>  6, 15 =>  7, 16 =>  8
+                0  => -8,  1 => -7, 2  => -6, 3  => -5, 4  => -4, 5  => -3, 6  => -2, 7  => -1,
+                8  =>  1,  9 =>  2, 10 =>  3, 11 =>  4, 12 =>  5, 13 =>  6, 14 =>  7, 15 =>  8
   						}
-  	@level_difference_uplift =[
-  	            3,10,40,90,160,250,360,490,640,810,1000,1210,1440,1690,1960,2250]
+  	@rank_points =
+  						{
+  							-8 => 0, -7 => 100, -6 => 200, -5 => 300, -4 => 400, -3 => 500, -2 => 600, -1 => 700,
+                1 => 800,  2 => 900, 3 => 1000, 4 => 1100, 5 => 1200, 6 => 1300, 7 => 1400, 8 => 1500
+  						}
+
 	end
 
 	def rank
@@ -22,41 +34,32 @@ class User
 		@progress
 	end
 
-	def boost_progress level 
+	def inc_progress kata
 
-		diff = @ranks[level] - @ranks[rank]
- 
-		case 
-		  when diff <= -2
-		  when diff == -1
-		  	@progress += 1
-		  else
-		  	@progress += @level_difference_uplift[diff]
-		end
+		#return kata < -8 || kata = 0 || kata > 8
+		raise "3 kinds of hell" if kata < -8 || kata == 0 || kata > 8
 
-	end
-
-	def inc_progress level
-
-		if @rank == 8
-			return
-		end
-
-		boost_progress level 
-	
-		if @progress >= 100  
-			
-			rank_jump = progress / 100
-			
-			old_rank_indicator = @ranks[@rank]
-			new_rank_indicator = [@ranks[@rank] + rank_jump,16].min
-
-			@rank = @to_ranks[new_rank_indicator] 
-			@progress -= (new_rank_indicator - old_rank_indicator)*100
-		end
-		 
-		if @rank == 8
+		return if rank >= 8
+#puts "------------"
+#puts "1    #{@ranks[@rank]}"
+#puts "2    #{@ranks[kata]}"
+		rank_difference = @ranks[kata] - @ranks[@rank]
+#puts "rank difference   #{rank_difference}"
+		return if rank_difference <= -2
+		new_points_total = @rank_points[@rank] + @progress + @level_difference_uplift[rank_difference]
+#puts " currrent progress total #{@progress}"
+#puts " currrent rank #{@rank}"
+#puts " uplift #{@level_difference_uplift[rank_difference]}"
+#puts " new points total #{new_points_total}" 
+#puts " divided by 100 #{new_points_total/100}"
+#puts " remainder formed #{new_points_total%100}"
+		if new_points_total >= 1500
+			@rank = 8
 			@progress = 0
+		else
+			@rank = @to_ranks[new_points_total/100]	
+			@progress = new_points_total%100
 		end
+#puts "final rank  #{@rank} progress  #{@progress}"
 	end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,18 +2,32 @@ require_relative "../lib/user"
 
 describe "User" do 
 
-	it "shouldn't fail at limit" do
+	it "Check initialization, termination on wrong kind of kata passed" do
 		user = User.new
 		expect(user.rank).to eq(-8)
 		expect(user.progress).to eq(0)
 
-		user.inc_progress(8)
-		expect(user.rank).to eq(8)
-		expect(user.progress).to eq(0)
-		#expect(user.rank).to eq(5)
+		user.inc_progress(0)
+		expect(user.rank).to raise_error("3 kinds of hell")
 	end
 
-	it "should progress" do
+	it "Progression" do
+		
+		user = User.new
+
+		user.inc_progress(-8)
+		expect(user.rank).to eq(-8)
+		expect(user.progress).to eq(3)
+
+		user.inc_progress(-8)
+		expect(user.rank).to eq(-8)
+		expect(user.progress).to eq(6)
+
+		user.inc_progress(7)
+		expect(user.rank).to eq(8)
+	end
+
+	it "should progress 2" do
   	user = User.new
 		expect(user.rank).to eq(-8)
 		expect(user.progress).to eq(0)
@@ -43,10 +57,26 @@ describe "User" do
 		expect(user.rank).to eq(8)
 		expect(user.progress).to eq(0)
 		#expect(user.rank).to eq(5)
-	end
+  end
+  	it "should progress 3" do
+  		user = User.new
 
-	
+  		user.inc_progress(-7)
+	  	expect(user.rank).to eq(-8)
+	  	expect(user.progress).to eq(10)
+
+	  	user.inc_progress(-6)
+	  	expect(user.rank).to eq(-8)
+	  	expect(user.progress).to eq(50)
+
+	  	user.inc_progress(-5)
+	  	expect(user.rank).to eq(-7)
+	  	expect(user.progress).to eq(40)
+
+	  	user.inc_progress(-4)
+	  	expect(user.rank).to eq(-6)
+	  	expect(user.progress).to eq(30)
+
+		end
 end
 
-#	user = User.new
-# Test.assert_equals(user.rank, -8)
